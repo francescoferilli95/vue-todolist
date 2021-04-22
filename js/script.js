13d9024b61b1ca182cb354cb18034561c2791baf
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-04-22 15:19:55 +0200

    populating todo list with vue

--- js/script.js
+++ js/script.js
@@ -7,7 +7,28 @@
 const app = new Vue({
     el: '#app',
     data: {
-
+        todos: [
+            {
+                text: 'Do my homework',
+                completed: false,
+            },
+            {
+                text: 'Study Vue',
+                completed: false,
+            },
+            {
+                text: 'Have some relax after homework',
+                completed: false,
+            },
+            {
+                text: 'Starting a new project',
+                completed: false,
+            },
+            {
+                text: 'Going out having a walk',
+                completed: false,
+            },
+        ],
     },
     methods: {
 
